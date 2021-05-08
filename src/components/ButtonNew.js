b4a136cd1fdd9e85c4820e3b62b8a20ec516d8b9
JosephSantos <joseph.ribeiro.santos@gmail.com>
2021-05-08 22:54:07 +0100

    Projeto atualizado com TabBar funcionando

--- src/components/ButtonNew.js
+++ src/components/ButtonNew.js
@@ -6,16 +6,32 @@ import { Icon, Entypo, Feather } from '@expo/vector-icons';
 
 export default function ButtonNew() {
     return (
-        <View style={styles.container}>
-            <LinearGradient style={styles.iconTabRound} start={{ x: 0, y: 1 }} end={{ x: 0, y: 0 }} colors={['#403D39', '#020E26']} >
-                <Feather name="edit" size={26} color={'#020E26'} />
-            </LinearGradient >
+        <View>
+            <LinearGradient style={styles.iconTabRound} start={{ x: 0, y: 1 }} end={{ x: 0, y: 0 }} colors={['#403D39', '#020E26']}>
+                <Feather name="plus" size={26} color='#FFF' />
+            </LinearGradient>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    iconTabRound: {
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        marginBottom: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 6,
+        shadowColor: '#020E26',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 5,
     }
 })
